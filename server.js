46a8ad4dbf86970b397f20646fbaa5f3b58ba488
Thais Zambe <thais@zambe.com.br>
2024-11-24 16:56:44 -0300

    Adicionando leves comentários

--- server.js
+++ server.js
@@ -5,27 +5,48 @@ app.use(express.json());
 
 /**
  * Retorna 404 - Not Found para o cliente.
- * @param {Response} res O objeto de resposta da requisição.
+ * @param {Response} res - O objeto de resposta da requisição.
  * @returns Uma imagem com tags HTML informando o código HTTP 404, que acompanha a resposta.
  */
 const retornaNotFound = (res) => res.status(404).send('<img src="https://http.cat/404">');
 
 /**
+ * Retorna 500 - Internal Server Error para o cliente.
+ * @param {Response} res - O objeto de resposta da requisição.
+ * @param {Error} [erro] - O erro inesperado (opcional), para log ou depuração.
+ * @returns Uma imagem com tags HTML informando o código HTTP 500, que acompanha a resposta.
+ */
+const retornaInternalServerError = (res, erro) => {
+    if (erro) {
+        console.error('Erro inesperado no servidor:', erro);
+    }
+    return res.status(500).send('<img src="https://http.cat/500">');
+};
+
+/**
  * Retorna todas as fotos existentes.
  */
 app.get('/posts', async (_, res) => {
-    res.status(200).json(await postsModel.get());
+    try {
+        res.status(200).json(await postsModel.get());
+    } catch (e) {
+        retornaInternalServerError(res, e);
+    }
 })
 
 /**
  * Retorna os dados da foto contendo o ID passado por parâmetro, e caso não encontre, 404.
  */
 app.get('/posts/:id(\\w+)', async (req, res) => {
-    const post = await postsModel.getOne(req.params.id);
-    if (post) {
-        res.status(200).json(post);
-    } else {
-        retornaNotFound(res);
+    try {
+        const post = await postsModel.getOne(req.params.id);
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            retornaNotFound(res);
+        }
+    } catch (e) {
+        retornaInternalServerError(res, e);
     }
 })
 
@@ -39,6 +60,6 @@ app.get('*', (_, res) => {
 /**
  * Inicia o servidor.
  */
-export const server = app.listen(process.env.PORT, () => {
+export const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Servidores escutando...');
 })
--- src/models/posts-model.js
+++ src/models/posts-model.js
@@ -4,27 +4,47 @@ import conectarAoBanco from '../config/db-config.js'
 
 const conexao = await conectarAoBanco(process.env.MONGODB_CONEXAO);
 
-export default {
-    get: async () => {
-        const db = conexao.db('imersao-instabytes');
-        const colecao = db.collection('posts');
-        return colecao.find().toArray();
-    },
-    getOne: async (id) => {
-        const db = conexao.db('imersao-instabytes');
-        const colecao = db.collection('posts');
-        const objID = ObjectId.createFromHexString(id);
-        return colecao.findOne({ _id: new ObjectId(objID) });
-    },
-    create: async (novoPost) => {
-        const db = conexao.db('imersao-instabytes');
-        const colecao = db.collection('posts');
-        return colecao.insertOne(novoPost);
-    },
-    update: async (id, novoPost) => {
-        const db = conexao.db('imersao-instabytes');
-        const colecao = db.collection('posts');
-        const objID = ObjectId.createFromHexString(id);
-        return colecao.updateOne({ _id: new ObjectId(objID) }, { $set: novoPost });
+const db = conexao.db(process.env.DB_NOME || 'imersao-instabytes');
+const colecao = db.collection(process.env.COLECAO_POSTS_NOME || 'posts');
+
+const toObjectId = (id) => {
+    if (!ObjectId.isValid(id)) {
+        throw new Error('Formato de ID inválido');
     }
+    return ObjectId.createFromHexString(id);
+};
+
+export default {
+    /**
+     * Recupera todos os documentos da coleção "posts" com paginação.
+     * @param {number} [page=1] - Número da página.
+     * @param {number} [limit=10] - Número de documentos por página.
+     * @returns {Promise<Array>} Uma promessa que resolve para os documentos encontrados.
+     */
+    get: async (page = 1, limit = 10) => {
+        const skip = (page - 1) * limit;
+        return colecao.find().skip(skip).limit(limit).toArray();
+    },
+
+    /**
+     * Recupera um documento específico da coleção "posts".
+     * @param {string} id - O ID do documento a ser recuperado.
+     * @returns {Promise<Object|null>} O documento encontrado ou null.
+     */
+    getOne: async (id) => colecao.findOne({ _id: toObjectId(id) }),
+    
+    /**
+     * Insere um novo documento na coleção "posts".
+     * @param {Object} novoPost - O objeto representando o novo documento.
+     * @returns {Promise<Object>} O resultado da operação de inserção.
+     */
+    create: async (novoPost) => colecao.insertOne(novoPost),
+
+    /**
+     * Atualiza um documento existente na coleção "posts".
+     * @param {string} id - O ID do documento a ser atualizado.
+     * @param {Object} novoPost - Os novos dados para o documento.
+     * @returns {Promise<Object>} O resultado da operação de atualização.
+     */
+    update: async (id, novoPost) => colecao.updateOne({ _id: toObjectId(id) }, { $set: novoPost })
 }
